eda2d005394f51c1826a8a07b62a4172df9f43cf
nicholassys <45427376+nicholassys@users.noreply.github.com>
2023-03-27 08:46:38 +0800

    Update BlockSimplifiedChinese.lua

--- BlockSimplifiedChinese.lua
+++ BlockSimplifiedChinese.lua
@@ -1,8 +1,5 @@
 local LPName, LPaddon = ...
 
--- 在此處添加全局變量
-local blockedWhispers = {}
-
 -- 創建一個配置介面
 local optionsPanel = CreateFrame("Frame", LPName.."OptionsPanel", InterfaceOptionsFramePanelContainer)
 optionsPanel.name = LPName
@@ -42,7 +39,7 @@ toggleButton:SetPoint("TOPLEFT", 16, -64)
 toggleButton.text:SetText("顯示小地圖按鈕")
 toggleButton.tooltipText = "啟用或停用簡體過濾小地圖按鈕"
 toggleButton:SetScript("OnClick", function(self)
-LPaddon.showButton = self:GetChecked()
+    LPaddon.showButton = self:GetChecked()
 LP_DB.showButton = self:GetChecked()
 if self:GetChecked() then
 myButton:Show()
@@ -132,193 +129,60 @@ end
 -- 載入 SavedVariables 中的設定
 LPaddon.checkMessage = LP_DB.enabled
 LPaddon.checkSender = LP_DB.checkSender
-LPaddon.showButton= LP_DB.showButton
+LPaddon.showButton = LP_DB.showButton
 LPaddon.disableInInstance = LP_DB.disableInInstance
 
--- 更新配置介面的開關狀態
+-- 更新配置介面的按鈕狀態
 messageToggleButton:SetChecked(LPaddon.checkMessage)
 senderToggleButton:SetChecked(LPaddon.checkSender)
+toggleButton:SetChecked(LPaddon.showButton)
 disableInInstanceToggleButton:SetChecked(LPaddon.disableInInstance)
 
--- 在此處調用初始化按鈕狀態的函數
+-- 註冊事件
+self:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+-- 初始化小地圖按鈕狀態
 InitializeButtonStatus()
 
-print("歡迎使用簡體過濾,輸入/bsc 或者 ESC→選項→插件 打開設定")
+-- 聊天過濾
+ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", filter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", filter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", filter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", filter)
 end
 
--- 保存插件設定時執行的函數
-local function OnSave(self, event, ...)
--- 保存設定到 SavedVariables
-LP_DB.enabled = LPaddon.checkMessage
-LP_DB.checkSender = LPaddon.checkSender
-LP_DB.showButton = LPaddon.showButton
-LP_DB.disableInInstance = LPaddon.disableInInstance
-end
-
--- 註冊事件
-local eventFrame = CreateFrame("Frame")
-eventFrame:RegisterEvent("ADDON_LOADED")
-eventFrame:RegisterEvent("PLAYER_LOGOUT")
-
--- 設定事件處理函數
-eventFrame:SetScript("OnEvent", function(self, event, ...)
-if event == "ADDON_LOADED" then
-local addon = ...
-if addon == LPName then
-OnLoad(self, event, ...)
-end
-elseif event == "PLAYER_LOGOUT" then
-OnSave(self, event, ...)
-end
-end)
-
--- 初始化
-LPaddon = {
-checkMessage = true,
-checkSender = true,
-showButton = true,
-filterWords = {},
-}
-
--- 添加要過濾的簡體字
-local function addWords(words)
-for word in string.gmatch(words, "[^%s,]+") do
-LPaddon.filterWords[word] = true
+local function OnEvent(self, event, ...)
+if event == "PLAYER_ENTERING_WORLD" then
+-- 刷新按鈕狀態
+InitializeButtonStatus()
 end
 end
 
--- 调用 addWords 函数添加关键字
-addWords("猫,枭")
+-- 注册事件处理器
+optionsPanel:SetScript("OnEvent", OnEvent)
+optionsPanel:RegisterEvent("ADDON_LOADED")
 
--- 註冊事件
-local events = {
-"CHAT_MSG_SAY",
-"CHAT_MSG_YELL",
-"CHAT_MSG_CHANNEL",
-"CHAT_MSG_EMOTE",
-"CHAT_MSG_WHISPER",
-}
-
-for _, v in pairs(events) do
-ChatFrame_AddMessageEventFilter(v, filter)
-end
-
--- 開啟拖動功能
-myButton:SetMovable(true)
-myButton:EnableMouse(true)
-myButton:SetScript("OnMouseDown", function(self, button)
+-- 設定小地圖按鈕點擊事件
+myButton:SetScript("OnClick", function(self, button)
 if button == "LeftButton" then
-self:StartMoving()
-end
-end)
-myButton:SetScript("OnMouseUp", function(self, button)
-if button == "LeftButton" then
-self:StopMovingOrSizing()
-end
-end)
-
-myButton:SetScript("OnClick", function(self, button, down)
-if button == "LeftButton" then
+InterfaceOptionsFrame_OpenToCategory(optionsPanel)
 InterfaceOptionsFrame_OpenToCategory(optionsPanel)
 elseif button == "RightButton" then
-InterfaceOptionsFrame_OpenToCategory(optionsPanel)
+LPaddon.checkMessage = not LPaddon.checkMessage
+LP_DB.enabled = LPaddon.checkMessage
+messageToggleButton:SetChecked(LPaddon.checkMessage)
 end
 end)
 
-myButton.tooltip = "簡體過濾"
+-- 設定小地圖按鈕的懸停提示
 myButton:SetScript("OnEnter", function(self)
-GameTooltip:SetOwner(self, "ANCHOR_TOPLEFT")
-GameTooltip:AddLine(self.tooltip)
+GameTooltip:SetOwner(self, "ANCHOR_LEFT")
+GameTooltip:AddLine("魔獸世界過濾簡體字插件", 1, 1, 1)
+GameTooltip:AddLine("左鍵：打開設置界面", 0.5, 0.5, 0.5)
+GameTooltip:AddLine("右鍵：開/關過濾", 0.5, 0.5, 0.5)
 GameTooltip:Show()
 end)
 myButton:SetScript("OnLeave", function(self)
 GameTooltip:Hide()
-end)
-
--- 添加顯示被過濾的耳語功能
-
--- 初始化被過濾的耳語存儲
-LPaddon.filteredWhispers = {}
-
--- 新建一個顯示被過濾耳語的框體
-local filteredWhispersFrame = CreateFrame("Frame", "FilteredWhispersFrame", UIParent, "BasicFrameTemplateWithInset")
-filteredWhispersFrame:SetSize(350, 400)
-filteredWhispersFrame:SetPoint("CENTER")
-filteredWhispersFrame:SetMovable(true)
-filteredWhispersFrame:EnableMouse(true)
-filteredWhispersFrame:RegisterForDrag("LeftButton")
-filteredWhispersFrame:SetScript("OnDragStart", filteredWhispersFrame.StartMoving)
-filteredWhispersFrame:SetScript("OnDragStop", filteredWhispersFrame.StopMovingOrSizing)
-filteredWhispersFrame:Hide()
-
--- 添加標題
-filteredWhispersFrame.Title = filteredWhispersFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-filteredWhispersFrame.Title:SetPoint("CENTER", filteredWhispersFrame.TitleBg, "CENTER", 0, 0)
-filteredWhispersFrame.Title:SetText("被過濾的耳語")
-
--- 添加滾動區域
-local whispersScrollArea = CreateFrame("ScrollFrame", nil, filteredWhispersFrame, "UIPanelScrollFrameTemplate")
-whispersScrollArea:SetSize(300, 300)
-whispersScrollArea:SetPoint("CENTER", filteredWhispersFrame, "CENTER", 0, -10)
-
--- 添加滾動區域的內容框
-local whispersScrollContent = CreateFrame("Frame", nil, whispersScrollArea)
-whispersScrollContent:SetSize(300, 300)
-whispersScrollArea:SetScrollChild(whispersScrollContent)
-
--- 設置添加被過濾的耳語的函數
-local function AddFilteredWhisper(sender, msg)
-table.insert(LPaddon.filteredWhispers, {sender = sender, msg = msg})
-local filteredWhisperText = whispersScrollContent:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-filteredWhisperText:SetPoint("TOPLEFT", whispersScrollContent, "TOPLEFT", 0, -10 - (#LPaddon.filteredWhispers - 1) * 20)
-filteredWhisperText:SetText(sender .. ": " .. msg)
-end
-
--- 修改過濾函數以保存被過濾的耳語
-local function filter(self, event, msg, sender)
--- ...其他原始過濾函數代碼...
-
-if LPaddon.checkSender then
-    for word in pairs(LPaddon.filterWords) do
-        if string.find(lowercase_name, word) then
-            if event == "CHAT_MSG_WHISPER" then
-                AddFilteredWhisper(sender, msg)
-            end
-            return true
-        end
-    end
-end
-end
-
--- 在耳語框創建一個圖標
-local whisperIcon = CreateFrame("Button", "WhisperIcon", ChatFrame1)
-whisperIcon:SetSize(18, 18)
-whisperIcon:SetPoint("TOPRIGHT", ChatFrame1, "TOPRIGHT", -35, -5)
-whisperIcon:SetNormalTexture("Interface\Icons\inv_misc_note_06")
-whisperIcon:SetHighlightTexture("Interface\Buttons\ButtonHilight-Square")
-whisperIcon:SetScript("OnClick", function()
-if filteredWhispersFrame:IsVisible() then
-    filteredWhispersFrame:Hide()
-else
-    filteredWhispersFrame:Show()
-end
-end)
-
--- 圖標提示
-whisperIcon.tooltipText = "顯示被過濾的耳語"
-whisperIcon:SetScript("OnEnter", function(self)
-GameTooltip:SetOwner(self, "ANCHOR_TOPLEFT")
-GameTooltip:AddLine(self.tooltipText)
-GameTooltip:Show()
-end)
-whisperIcon:SetScript("OnLeave", function(self)
-GameTooltip:Hide()
-end)
-
--- 添加一個關閉按鈕到被過濾耳語框
-local closeFilteredWhispersButton = CreateFrame("Button", nil, filteredWhispersFrame, "UIPanelCloseButton")
-closeFilteredWhispersButton:SetPoint("TOPRIGHT", filteredWhispersFrame, "TOPRIGHT", -6, -6)
-closeFilteredWhispersButton:SetScript("OnClick", function()
-filteredWhispersFrame:Hide()
 end)
 
